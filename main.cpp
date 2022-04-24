6f5ea8e71bc3eab5ed3af0336ddba93b08412046
CDwee <96454637+CDwee@users.noreply.github.com>
2022-04-24 11:59:31 -0500

    Update and rename README.md to main.cpp

---
+++ main.cpp
@@ -0,0 +1,87 @@
+// Started at 7:44 4-24-2022
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   int sixty = 60;
+   int negativeValue = -80;
+
+   float number = 90.80;
+   double number2 = 7.7;
+
+   float sum = sixty + number + number2 + 100;
+   int subtract = sixty - 10;
+   int multiply = sixty * 100;
+   int divide =  sixty / 10;
+
+   cout<< divide;
+
+return 0;
+}
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+
+   string fruit = "apple";
+   string longString = "i am a very long string example";
+
+   cout<<fruit;
+
+   return 0;
+}
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+   string name;
+   int a;
+   int b;
+   cout<< "Enter your name: ";
+   cin >> name;
+
+   cout<< "Enter value of a: ";
+   cin >> a;
+
+   cout<< "Enter value of b: ";
+   cin >> b;
+
+   cout<<"Your name is: " << name<< endl;
+   cout<<"Sum of a and b is: " << a+b;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for(int i = 0; i < 10; i++) {
+        
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
+
+#include <iostream>
+
+int main()
+{
+    int favorite_number;
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    
+    return 0;
+}
+
+// Ended at 11:59 4-24-2022
